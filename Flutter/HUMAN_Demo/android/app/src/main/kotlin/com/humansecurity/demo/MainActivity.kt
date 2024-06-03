52dddff7121c9076a9d43b814ef240f422de3f99
Oren Yaar <oren.yaar@humansecurity.com>
2024-06-03 17:44:43 +0300

    Flutter Android demo app v4

--- Flutter/HUMAN_Demo/android/app/src/main/kotlin/com/humansecurity/demo/MainActivity.kt
+++ Flutter/HUMAN_Demo/android/app/src/main/kotlin/com/humansecurity/demo/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.perimeterx.perimeterx
 
 import androidx.annotation.NonNull
-import com.perimeterx.mobile_sdk.PerimeterX
-import com.perimeterx.mobile_sdk.PerimeterXChallengeResult
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-import org.json.JSONObject
 
 class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.perimeterx.demo/perimeterx").setMethodCallHandler {
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.humansecurity.demo/human").setMethodCallHandler {
                 call, result ->
-            PerimeterxManager.handleEvent(call, result)
+            HumanManager.handleEvent(call, result)
         }
     }
 }
